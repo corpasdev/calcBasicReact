bf043eab34615029168f91aca94323be5171b96d
corpasdev <brandoncorpas01@gmail.com>
2023-10-20 08:15:35 -0500

    UI mejorada con colores distintivos

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,12 @@ function App() {
     setValueExp(valueExp.slice(0, -1));
   }
 
+  const getResult = () => {
+    alert("Resultado!")
+  }
+
   return (
-    <main className="">
+    <main className="bg-neutral-50 rounded p-12">
       <input value={valueExp} placeholder="0" className="mb-6 py-2 rounded border-gray-200 border-2 pr-2 text-2xl w-full font-bold text-end" type="text" readOnly/>
       <div className="flex gap-12">
         <div className="grid w-56 grid-cols-3 gap-4">
@@ -32,6 +36,7 @@ function App() {
           <Button fns={{setExpresion}} value={"."}/>
           <Button fns={{clearEnd}} value={"D"}/>
           <Button fns={{clear}} value={"C"}/>
+          <Button fns={{getResult}} value={"="}/>
         </div>
         <div className="flex flex-col gap-4">
           {
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 
 export const Button = ({value, fns}) => {
-    const {setExpresion, clear, clearEnd} = fns;
+    const {setExpresion, clear, clearEnd, getResult} = fns;
 
     const handleFunction = (value) => {
         switch(value) {
@@ -10,6 +10,9 @@ export const Button = ({value, fns}) => {
             case 'C':
                 clear();
                 break;
+            case '=':
+                getResult();
+                break;
             default:
                 setExpresion(value);
         }
@@ -17,10 +20,28 @@ export const Button = ({value, fns}) => {
 
     const OPERADORES = ['+', '-', '×', '÷', '^'];
 
+    let styleAdd;
+
+    switch(value) {
+        case 'C': 
+            styleAdd =  "col-span-2 bg-blue-700";
+            break;
+        case 'D': 
+            styleAdd = "bg-red-500";
+            break;
+        case '=': 
+            styleAdd = "bg-lime-500";
+            break;
+        default: 
+            styleAdd = "";
+    }
+
+    OPERADORES.includes(value)? styleAdd = "bg-orange-600": null;
+
     return (
         <button 
         onClick={() => handleFunction(value)} 
-        className="flex justify-center items-center text-white bg-sky-500 rounded font-bold bg-gray-200 px-2 py-2" 
+        className={"flex justify-center items-center text-white bg-sky-500 rounded font-bold bg-gray-200 px-2 py-2 "+styleAdd} 
         style={OPERADORES.includes(value)? {fontSize: "20px", width: "60px"}: null}
         >
             {value}
